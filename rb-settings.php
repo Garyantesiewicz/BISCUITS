b5c4067d67052ecedf423cb5f111242946e4a3e9
Garyantesiewicz <77463295+Garyantesiewicz@users.noreply.github.com>
2024-02-07 15:06:30 +0000

    Install & Backup files

---
+++ rb-settings.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('rb-plugins/nginx-compatibility/nginx-compatibility.php');
+require_once('rb-plugins/sso/sso.php');
+require_once('rb-plugins/nginx-cache/nginx-cache.php');
+require_once('rb-plugins/disable-wordpress-core-update/disable-wordpress-core-update.php');
+require_once('rb-plugins/wp-security/wp-security.php');
+require_once('rb-plugins/raidboxes-site-health-ssl/raidboxes-site-health-ssl.php');
+require_once('rb-plugins/log-login-attempts/log-login-attempts.php');
+require_once('rb-plugins/raidboxes-admin-menu/raidboxes-admin-menu.php');
+
+?>
